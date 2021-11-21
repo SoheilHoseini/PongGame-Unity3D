584ee700a244bcbc4c2ae93cb9836032ae93da85
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-11-21 18:41:12 +0330

    Movements and score ui

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,15 +6,21 @@ public class Ball : MonoBehaviour
 {
     [SerializeField] int lifeCnt;
     [SerializeField] float ballSpeed = 0.3f;
+    [SerializeField] UnityEngine.UI.Text lifeCntText;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         lifeCnt = 3;
+        float xDir = Random.Range(0, 2) == 0 ? -1 : 1;
+        float yDir = Random.Range(0, 2) == 0 ? -1 : 1;
+
+
+        Vector3 move = new Vector3(xDir * ballSpeed, yDir * ballSpeed, 0f);
+        GetComponent<Rigidbody>().velocity = move;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         if(lifeCnt <= 0)
@@ -23,13 +29,16 @@ public class Ball : MonoBehaviour
         }
 
     }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Death"))
+        if(other.gameObject.CompareTag("Death"))
         {
             lifeCnt--;
+            lifeCntText.text = "Lifes: " + lifeCnt.ToString();
         }
     }
+
     //When the player loses for 3 times, it is Game Over!
     void LoseProcess()
     {
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,10 +7,13 @@ public class Main : MonoBehaviour
     [SerializeField] float speed = 3f;
     [SerializeField] float movingAmount = 0.2f;
     [SerializeField] bool MainPlayer;
-
+    [SerializeField] UnityEngine.UI.Text scoreText;
+    private int score;
     // Update is called once per frame
     void Update()
     {
+        score = 0;
+
         //Moving Up
         if(Input.GetKey(KeyCode.UpArrow))
         {
@@ -35,6 +38,16 @@ public class Main : MonoBehaviour
             {
                 gameObject.transform.Translate(0f, movingAmount * speed, 0f);
             }
+        }
+
+       
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ball"))
+        {
+            score++;
+            scoreText.text = "Score: " + score.ToString();
         }
     }
 }
