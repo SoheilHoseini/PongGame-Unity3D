51377089e498813692da610582927c1753a3e458
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-11-21 17:18:57 +0330

    Project initiated

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] int lifeCnt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeCnt = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lifeCnt <= 0)
+        {
+            LoseProcess();
+        }
+
+    }
+
+    //When the player loses for 3 times, it is Game Over!
+    void LoseProcess()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] float speed = 0.3f;
+    [SerializeField] float movingAmount = 0.2f;
+    [SerializeField] bool MainPlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moving Up
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            if(MainPlayer == true)
+            {
+                gameObject.transform.Translate(0f, movingAmount * speed * Time.deltaTime, 0f);
+            }
+            else
+            {
+                gameObject.transform.Translate(0f, -1 * movingAmount * speed * Time.deltaTime, 0f);
+            }
+        }
+
+        //Moving Down
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            if (MainPlayer == true)
+            {
+                gameObject.transform.Translate(0f, -1 * movingAmount * speed * Time.deltaTime, 0f);
+            }
+            else
+            {
+                gameObject.transform.Translate(0f, movingAmount * speed * Time.deltaTime, 0f);
+            }
+        }
+    }
+}
