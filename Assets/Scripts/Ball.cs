e14e304ddac8cfe81ba78d23cbdc3340353b7f2b
Soheil Hoseini <soheil.hosseini78@gmail.com>
2021-11-21 19:27:37 +0330

    Final

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,7 +7,7 @@ public class Ball : MonoBehaviour
     [SerializeField] int lifeCnt;
     [SerializeField] float ballSpeed = 0.3f;
     [SerializeField] UnityEngine.UI.Text lifeCntText;
-
+    [SerializeField] UnityEngine.UI.Text gameOver;
 
     void Start()
     {
@@ -18,6 +18,7 @@ public class Ball : MonoBehaviour
 
         Vector3 move = new Vector3(xDir * ballSpeed, yDir * ballSpeed, 0f);
         GetComponent<Rigidbody>().velocity = move;
+        
     }
 
 
@@ -28,6 +29,8 @@ public class Ball : MonoBehaviour
             LoseProcess();
         }
 
+        else
+        { gameOver.text = ""; }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -36,12 +39,32 @@ public class Ball : MonoBehaviour
         {
             lifeCnt--;
             lifeCntText.text = "Lifes: " + lifeCnt.ToString();
+            gameObject.transform.position = new Vector3(0f, 0f, 0f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 currentMove = GetComponent<Rigidbody>().velocity;
+
+        ////GetComponent<Rigidbody>().velocity = new Vector3(currentMove.x, -currentMove.y, 0f);
+        //GetComponent<Rigidbody>().AddForce(currentMove.x , -currentMove.y *10, 0f);
+
+        if (collision.collider.CompareTag("Player"))
+        {
+            Vector3 vel;
+            vel.x = currentMove.x;
+            vel.y = (currentMove.y / 2) + (collision.collider.attachedRigidbody.velocity.y / 3);
+            vel.z = 0f;
+            //rb2d.velocity = vel;
+            GetComponent<Rigidbody>().velocity = vel;
         }
     }
 
     //When the player loses for 3 times, it is Game Over!
     void LoseProcess()
     {
-
+        gameOver.text = "Game Over!";
+        lifeCnt = 3;
     }
 }
